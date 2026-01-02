787ebbaefc6a7aac7dbd1f295a464a677eedcc76
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-02 15:45:53 +0600

    реализовано основные функций с добавлением удалением товары в корзину с provider

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,3 +1,24 @@
+import 'package:ema_store/models/product.dart';
 import 'package:flutter/material.dart';
 
-class CartProvider extends ChangeNotifier {}
+class CartProvider extends ChangeNotifier {
+  List<ProductModel> _cartItems = [];
+
+  // getter
+
+  List<ProductModel> get cartItems => _cartItems;
+
+  // метод добавление товара в корзину
+
+  void addProductItem(ProductModel product) {
+    _cartItems.add(product);
+    notifyListeners();
+  }
+
+  // метод удаление товара из корзины
+
+  void removeFromCart(ProductModel product) {
+    _cartItems.remove(product);
+    notifyListeners();
+  }
+}
