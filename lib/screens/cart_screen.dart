f3c64ce99c8e81e7f80b5fbe5ffd12f892bea01c
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-02 21:37:21 +0600

    обновление UI

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,13 +1,20 @@
+import 'package:ema_store/models/product.dart';
+import 'package:ema_store/providers/product_provider.dart';
 import 'package:ema_store/theme/app_colors.dart';
 import 'package:ema_store/theme/app_text_styles.dart';
 import 'package:ema_store/widgets/app_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final productProvider = context.watch<ProductProvider>();
+
+    final products = productProvider.products;
+
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackround,
       appBar: _appBar(context),
@@ -30,27 +37,7 @@ class CartScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              ListView.builder(
-                itemCount: 20,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 8.0),
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(color: Colors.red),
-                    child: Row(
-                      children: [
-                        Image.network(
-                          "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-                          height: 80,
-                          width: 80,
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+              _buildCartProductListView(products),
             ],
           ),
         ),
@@ -131,6 +118,92 @@ class CartScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCartProductListView(List<ProductModel> products) {
+    return ListView.builder(
+      itemCount: products.length,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 8.0),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(color: AppColors.cardBackround),
+          child: Row(
+            children: [
+              Image.network(product.image, height: 80, width: 80),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        product.title,
+                        style: AppTextStyles.descriptionTextStyle,
+                      ),
+                      Text(
+                        product.category.toUpperCase(),
+                        style: AppTextStyles.cartCategoryTextStyles,
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        children: [
+                          _buildQtyButton("-", () {}),
+                          const SizedBox(width: 20.0),
+                          Text("0", style: AppTextStyles.descriptionTextStyle),
+                          const SizedBox(width: 20.0),
+                          _buildQtyButton("+", () {}),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Text(
+                    "\$${product.price}",
+                    style: AppTextStyles.priceTextStyle,
+                  ),
+                  const SizedBox(height: 10),
+                  // Иконка мусорки
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.delete_outline, color: Colors.grey),
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildQtyButton(String text, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 40,
+        height: 40,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(text, style: const TextStyle(fontSize: 18)),
       ),
     );
   }
