35d84d726fc8354847475e90a9549fb5845479f9
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-05 21:38:20 +0600

    feat(widget-catalogHeader): update widgets added function for clear all products

--- lib/widgets/catalog_carts_header.dart
+++ lib/widgets/catalog_carts_header.dart
@@ -2,20 +2,26 @@ import 'package:ema_store/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CatalogCartsHeader extends StatelessWidget {
-  const CatalogCartsHeader({super.key});
+  final String catalogMainText;
+  final String clearCartText;
+  final void Function()? onTap;
+  const CatalogCartsHeader({super.key, this.onTap, required this.catalogMainText, required this.clearCartText});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text("Корзина", style: AppTextStyles.mainTextStyles),
-        Text(
-          "Очистить все",
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 15,
-            fontWeight: FontWeight.w600,
+        Text(catalogMainText, style: AppTextStyles.mainTextStyles),
+        GestureDetector(
+          onTap: onTap,
+          child: Text(
+            clearCartText,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ],
